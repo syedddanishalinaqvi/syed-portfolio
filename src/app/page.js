d15fd59c3359c13a483cda7127f2ec5c1726635a
syedddanishalinaqvi <69369581+syedddanishalinaqvi@users.noreply.github.com>
2023-12-29 04:55:52 +0530

    removed 1 element

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <>
       <div style={{ display: showSplash ? 'flex' : 'none' }} className="splash-screen">
-        Syed Dansih Ali Naqvi<p>-Portfolio</p>
+        Syed Dansih Ali Naqvi
       </div>
       <div style={{ display: showSplash ? 'none' : '' }} className='homepage'>
         <div className="border">
